31f7351a16014f7504458ef4de74af534dcbcd1d
zevixdigital <zevixdigital@gmail.com>
2026-06-25 13:20:17 +0530

    Update HeroSection.tsx

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -164,14 +164,14 @@ export default function HeroSection() {
             </div>
             <div className="flex flex-wrap gap-2.5">
               <a 
-                href="https://drive.google.com/file/d/1-bGXqtQPvixKyRYFqovw6ptiY5u4-tVY/view?usp=sharing" 
+                href="https://drive.google.com/file/d/1YWs3ybRv2o5gh_-5u9PauHpEtJqhyP1J/view?usp=sharing" 
                 download
                 className="inline-flex items-center gap-1.5 bg-white hover:bg-orange-50/50 border border-slate-200 hover:border-[#f37022] text-slate-700 hover:text-[#f37022] px-4 py-2 rounded-xl text-xs font-semibold shadow-sm transition-all"
               >
                 <Download className="w-3.5 h-3.5" /> Participant Entry Form
               </a>
               <a 
-                href="https://drive.google.com/file/d/1d7RT6YhCm0iaBf7yJS91M_WOJN0KQkWE/view?usp=sharing" 
+                href="https://drive.google.com/file/d/1ouCUiypAvPzE0-_ldnJypOsOaMest2ZG/view?usp=sharing" 
                 download
                 className="inline-flex items-center gap-1.5 bg-white hover:bg-orange-50/50 border border-slate-200 hover:border-[#f37022] text-slate-700 hover:text-[#f37022] px-4 py-2 rounded-xl text-xs font-semibold shadow-sm transition-all"
               >
